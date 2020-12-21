edf1459eb5fda4de801da78be9daec8156a2702f
Mattis <mattis99@hotmail.de>
2020-12-21 15:29:18 +0100

    added welcome.php

---
+++ Aufgabe2/cgi-bin/welcome.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your Birthdate: <?php echo $_POST["geburtsdatum"]; ?><br>
+Your Text: <?php echo $_POST["area"]; ?>
+
+
+</body>
+</html> 
